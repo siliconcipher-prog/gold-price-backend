ac52d778c537a5e3d3c31be7e8ffb1932533965e
siliconcipher <siliconcipher@gmail.com>
2026-02-11 17:28:36 +0530

    Update cron job timing

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ ALLOWED_ORIGINS = (
     else [
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "http://192.168.1.10:5500",
+        "http://192.168.1.6:5500",
     ]
 )
 
